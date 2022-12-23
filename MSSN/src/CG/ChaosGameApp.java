ad3b1eeb007d62912e53ec21f1a66b6456e80e0e
Tomás Matos <80894523+TomasMBMatos@users.noreply.github.com>
2022-12-23 20:24:02 +0000

    All files updated

---
+++ MSSN/src/CG/ChaosGameApp.java
@@ -0,0 +1,56 @@
+package CG;
+
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+import processing.core.PVector;
+import setup.IProcessingApp;
+
+public class ChaosGameApp implements IProcessingApp {
+    ArrayList<PVector> initialPoints;
+    PVector currentPoint;
+
+    @Override
+    public void setup(PApplet p) {
+        initShape(p);
+        drawInitialRandomPoint(p);
+    }
+
+    private void initShape(PApplet p) {
+        initialPoints = new ArrayList<PVector>();
+        initialPoints.add(new PVector(p.width / 2, 0));
+        initialPoints.add(new PVector(0, p.height));
+        initialPoints.add(new PVector(p.width, p.height));
+        for(PVector p1 : initialPoints) {
+            p.point(p1.x, p1.y);
+        }
+    }
+
+    private void drawInitialRandomPoint(PApplet p) {
+        PVector randomInitialPoint = new PVector(p.random(p.width), p.random(p.height));
+        p.point(randomInitialPoint.x, randomInitialPoint.y);
+        currentPoint = randomInitialPoint;
+    }
+
+    @Override
+    public void draw(PApplet p, float dt) {
+        for(int i = 0; i != 1000; ++i) {
+            int r = (int) p.random(initialPoints.size());
+            float x = PApplet.lerp(currentPoint.x, initialPoints.get(r).x, 0.5f);
+            float y = PApplet.lerp(currentPoint.y, initialPoints.get(r).y, 0.5f);
+            p.point(x, y);
+            currentPoint = new PVector(x, y);
+        }
+    }
+
+    @Override
+    public void mousePressed(PApplet p) {
+        
+    }
+
+    @Override
+    public void keyPressed(PApplet p) {
+
+    }
+
+}
--- MSSN/src/fractals/MandelbrotSet.java
+++ MSSN/src/fractals/MandelbrotSet.java
@@ -19,18 +19,26 @@ public class MandelbrotSet {
     }
 
     public void display(PApplet p, SubPlot plt) {
+        int tt = p.millis();
         p.loadPixels();
         for(int xx=x0;xx<x0+dimx;xx++) {
             for(int yy=y0;yy<y0+dimy;yy++) {
                 double[] cc = plt.getWorldCoord(xx, yy);
                 Complex c = new Complex(cc);
                 Complex x = new Complex();
+                Complex y = new Complex(0, 0.8);
                 int i;
                 for(i=0;i<nIter;i++) {
                     x.mult().add(c);
-                    if(x.norm() > 2) break;
+                    if(x.norm() > 16) break;
                 }
+                p.colorMode(PApplet.RGB,255);
                 p.pixels[yy*p.width+xx] = (i == nIter) ? p.color(0) : p.color((i % 16)*16);
+                p.colorMode(PApplet.HSB,255);
+                if((i % 16)*16 > 130) {
+                    float hue = PApplet.map((i % 16)*15, 130, 255, 0, 255);
+                    p.pixels[yy*p.width+xx] = (i == nIter) ? p.color(0) : p.color(hue,255,255);
+                }
                 // Slow Method
                 //int color = (i == nIter) ? p.color(0) : p.color((i % 16)*16);
                 //p.stroke(color);
@@ -38,5 +46,6 @@ public class MandelbrotSet {
             }
         }
         p.updatePixels();
+        System.out.println(p.millis()-tt);
     }
 }
